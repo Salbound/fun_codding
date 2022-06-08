859d7e6f9133935512a7de7e4890dbb17c0857d1
Salbound <luiz_hen33@hotmail.com>
2022-06-07 21:17:10 -0300

    new section

--- Python/03_Advanced/04_Sets.py
+++ Python/03_Advanced/04_Sets.py
@@ -1,4 +1,3 @@
 #https://www.youtube.com/watch?v=Qs3BSFZnZSI&list=PLqnslRFeH2UqLwzS0AwKDKLrpYBKzLBy2&index=4&ab_channel=PythonEngineer
 
-# Sets: unordered, mutable, no duplicates
-
+# Sets: unordered, mutable, no duplicatesc
