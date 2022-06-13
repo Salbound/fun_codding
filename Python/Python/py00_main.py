36879ca60ef5d1306fbb84c41f27835001103bc5
Luiz Henrique <luiz_hen33@hotmail.com>
2022-06-13 19:23:25 +0000

    Add loop

--- Python/Python/py00_main.py
+++ Python/Python/py00_main.py
@@ -5,6 +5,7 @@ from py01_basic_syntax import basicSyntax
 from py02_variable import variable
 from py03_basic_operator import basicOperator
 from py04_decision import decisaoIfElse
+from py05_loop import loops
 
 def run():
     '''Conceitos básicos de Python, diferença de palavra/texto/paragrafo em uma var string e informar valores de entrada.'''
@@ -21,13 +22,13 @@ def run():
     # var.converter_variavel_para_outra()
 
     '''Conceitos básicos sobre operadores.
-    Operações básicas: + - * / % ** //
-    Comparação: == != < > >= <=
-    Assignments: = += -= *= /= %= **= //=
-    Logicos:
-        Lógicos: and or not
-        Membership: in not in
-        Identiyy: is is not'''
+            Operações básicas: + - * / % ** //
+            Comparação: == != < > >= <=
+            Assignments: = += -= *= /= %= **= //=
+            Logicos:
+                Lógicos: and or not
+                Membership: in not in
+                Identiyy: is is not'''
     # oper = basicOperator
     # oper.basico()
     # oper.comparacao()
@@ -40,9 +41,19 @@ def run():
     # seSenao.ifelifelse()
     # seSenao.if_dentroDeIf()
 
-    '''Conceitos básicos de loop (while, for for_dentro_for).'''
-
-
+    '''Conceitos básicos de loop (while e for).
+            break: interrompe próximas iterações do for/while
+            continue: quebra essa iteração e vai direto para próxima iteração
+            pass: não faz nada relacionado a essa iteração, e pode executar demais códigos dentro no local'''
+    # loop = loops
+    # loop.while_enquanto()
+    # loop.while_comElse()
+    # loop.for_para()
+    # loop.for_complex_example_prime_number()
+    # loop.while_complex_example_prime_number()
+    # loop.ex_break()
+    # loop.ex_continue()
+    # loop.ex_pass()
 
 if __name__ == "__main__":
     run()
--- Python/Python/py05_loop.py
+++ Python/Python/py05_loop.py
@@ -4,7 +4,86 @@ import sys
 
 class loops:
     def while_enquanto ():
-        pass
+        count = 0
+        while (count < 9):
+            print (f'The count is: {count}')
+            count = count + 1
+
+    def while_comElse ():
+        count = 0
+        while count < 5:
+            print (f"{count} is  less than 5")
+            count = count + 1
+        else:
+            print (f"{count} is not less than 5")
+
+    def for_para ():
+        # For informar o próximo nível abaixo. Passandro uma String, é separado por letra.
+        for letter in 'Python':
+            print ('Current Letter :', letter)
+
+        # Passando uma lista, o próximo nível é a String.
+            fruits = ['banana', 'apple',  'mango']
+        for fruit in fruits:        # Second Example
+            print ('Current fruit :', fruit)
+
+    def for_complex_example_prime_number ():
+        for num in range(10,20):            #to iterate between 10 to 20
+            for i in range(2,num):          #to iterate on the factors of the number
+                if num%i == 0:              #to determine the first factor
+                    j=num/i                 #to calculate the second factor
+                    print (f'{num} equals {i} * {j}')
+                    break                   #to move to the next number, the #first FOR
+            else:                           # else part of the loop
+                print (f'{num} is a prime number')
+                break
+
+    def while_complex_example_prime_number ():
+        i = 2
+        while(i < 100):
+            j = 2
+            while(j <= (i/j)):
+                if not(i%j): break
+                j = j + 1
+            if (j > i/j) : print (f"{i} is prime.")
+            i = i + 1
+
+    def ex_break ():
+        for letter in 'Python':     # First Example
+            if letter == 'h':
+                break
+            print (f'Current Letter : {letter}')
+            
+            var = 10                    # Second Example
+            while var > 0:              
+                # print (f'Current variable value : {var}')
+                var = var -1
+                if var == 5:
+                    break
+        print("\n")
+
+    def ex_continue ():
+        for letter in 'Python':     # First Example
+            if letter == 'h':
+                continue
+            print (f'Current Letter : {letter}')
+
+            var = 10                    # Second Example
+            while var > 0:              
+                var = var -1
+                if var == 5:
+                    continue
+                # print (f'Current variable value : {var}')
+        print("\n")
+
+    def ex_pass ():
+        for letter in 'Python': 
+            if letter == 'h':
+                pass
+                print ('This is pass block')
+            print (f'Current Letter : {letter}')
+        print("\n")
+
 
 
 if __name__ == "__main__":
