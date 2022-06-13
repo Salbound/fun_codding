e0f3d138af2347e6f50e5b12ded7635e2a6e8adb
Luiz Henrique <luiz_hen33@hotmail.com>
2022-06-13 16:36:51 +0000

    Add Basic Syntax

---
+++ Python/Python/basic_syntax.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+import sys
+
+class basicSyntax:
+    def string_variables ():
+        word = 'word\n'
+        sentence = "This is a sentence.\n"
+        paragraph = """This is a paragraph. It is
+made up of multiple lines and sentences.\n"""
+        print(word, sentence, paragraph)
+    
+    # First comment
+    '''
+    This is a multiline
+    comment.
+    '''
+
+    def inputs ():
+        resposta = input("\nDigite seu nome: ")
+        #on python 2.* it's used 'raw_input()'
+        print(f"Respostas: {resposta}")
+
+
+
+if __name__ == "__main__":
+    print("Você não deveria estar aqui. Execute o 'main.py' da pasta raiz.")
+    sys.exit()
