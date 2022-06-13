1e5c18773b9ff7f6f6a4d17f92ca3db6bc7a917c
Luiz Henrique <luiz_hen33@hotmail.com>
2022-06-13 17:42:05 +0000

    Add operatores

--- Python/Python/py00_main.py
+++ Python/Python/py00_main.py
@@ -3,6 +3,7 @@
 import sys
 from py01_basic_syntax import basicSyntax
 from py02_variable import variable
+from py03_basic_operator import basicOperator
 
 def run():
     '''Conceitos básicos de Python, diferença de palavra/texto/paragrafo em uma var string e informar valores de entrada.'''
@@ -17,6 +18,20 @@ def run():
     # var.var_tuple()
     # var.var_dicionario()
     # var.converter_variavel_para_outra()
+
+    '''Conceitos básicos sobre operadores.
+    Operações básicas: + - * / % ** //
+    Comparação: == != < > >= <=
+    Assignments: = += -= *= /= %= **= //=
+    Logicos:
+        Lógicos: and or not
+        Membership: in not in
+        Identiyy: is is not'''
+    # oper = basicOperator
+    # oper.basico()
+    # oper.comparacao()
+    # oper.assignment_operators()
+    # oper.identity()
 
 if __name__ == "__main__":
     run()
---
+++ Python/Python/py03_basic_operator.py
@@ -0,0 +1,129 @@
+#!/usr/bin/python
+
+import sys
+
+class basicOperator:
+    def basico ():
+        a = 21
+        b = 10
+        c = 0
+
+        c = a + b
+        print ("Line 1 - Value of c is ", c)
+
+        c = a - b
+        print ("Line 2 - Value of c is ", c )
+
+        c = a * b
+        print ("Line 3 - Value of c is ", c )
+
+        c = a / b
+        print ("Line 4 - Value of c is ", c )
+
+        c = a % b
+        print ("Line 5 - Value of c is ", c)
+
+        a = 2
+        b = 3
+        c = a**b 
+        print ("Line 6 - Value of c is ", c)
+
+        a = 10
+        b = 5
+        c = a//b 
+        print ("Line 7 - Value of c is ", c)
+
+    def comparacao ():
+        a = 21
+        b = 10
+        c = 0
+
+        if ( a == b ):
+            print ("Line 1 - a is equal to b")
+        else:
+            print ("Line 1 - a is not equal to b")
+
+        if ( a != b ):
+            print ("Line 2 - a is not equal to b")
+        else:
+            print ("Line 2 - a is equal to b")
+
+        if ( a < b ):
+            print ("Line 4 - a is less than b" )
+        else:
+            print ("Line 4 - a is not less than b")
+
+        if ( a > b ):
+            print ("Line 5 - a is greater than b")
+        else:
+            print ("Line 5 - a is not greater than b")
+
+        a = 5
+        b = 20
+        if ( a <= b ):
+            print ("Line 6 - a is either less than or equal to  b")
+        else:
+            print ("Line 6 - a is neither less than nor equal to  b")
+
+        if ( b >= a ):
+            print ("Line 7 - b is either greater than  or equal to b")
+        else:
+            print ("Line 7 - b is neither greater than  nor equal to b")
+
+
+    def assignment_operators ():
+        a = 21
+        b = 10
+        c = 0
+
+        c = a + b
+        print ("Line 1 - Value of c is ", c)
+
+        c += a
+        print ("Line 2 - Value of c is ", c)
+
+        c *= a
+        print ("Line 3 - Value of c is ", c)
+
+        c /= a 
+        print ("Line 4 - Value of c is ", c)
+
+        c  = 2
+        c %= a
+        print ("Line 5 - Value of c is ", c)
+
+        c **= a
+        print ("Line 6 - Value of c is ", c)
+
+        c //= a
+        print ("Line 7 - Value of c is ", c)
+
+    def identity ():
+        a = 20
+        b = 20
+
+        if ( a is b ):
+            print ("Line 1 - a and b have same identity")
+        else:
+            print ("Line 1 - a and b do not have same identity")
+
+        if ( id(a) == id(b) ):
+            print ("Line 2 - a and b have same identity")
+        else:
+            print ("Line 2 - a and b do not have same identity")
+
+        b = 30
+        if ( a is b ):
+            print ("Line 3 - a and b have same identity")
+        else:
+            print ("Line 3 - a and b do not have same identity")
+
+        if ( a is not b ):
+            print ("Line 4 - a and b do not have same identity")
+        else:
+            print ("Line 4 - a and b have same identity")
+
+
+if __name__ == "__main__":
+    print("Você não deveria estar aqui. Execute o 'main.py' da pasta raiz.")
+    sys.exit()
